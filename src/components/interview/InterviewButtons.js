7057ae2636ca1a61faa3ca90fbfd8ebbddc9eacc
David <david.d.hernandez@accenture.com>
2023-03-11 12:28:14 -0600

    Bug fixed

--- src/components/interview/InterviewButtons.js
+++ src/components/interview/InterviewButtons.js
@@ -1,3 +1,4 @@
+import * as React from "react";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -5,44 +6,27 @@ import FormControl from "@mui/material/FormControl";
 import { useEffect, useState } from "react";
 
 export const InterviewButtons = ({ questionNumber }) => {
-  const [checked, setChecked] = useState(false);
-  const [change, setChange] = useState(false);
-  const [initialized, setInitialized] = useState(false);
+  const [value, setValue] = useState(null);
 
   useEffect(() => {
-    if (!initialized) return setInitialized(true);
-    setChange(false);
-  }, [questionNumber, initialized]);
+    setValue(null);
+  }, [questionNumber]);
 
-  const handleOnClick = () => {
-    console.log()
-    setChange(true);
-    setChecked((prevState) => {
-      return !prevState;
-    });
+  const handleChange = (event) => {
+    setValue(event.target.value);
   };
 
   return (
     <FormControl>
       <RadioGroup
+        aria-labelledby="radio-buttons-group"
+        name="radio-buttons-group"
+        value={value}
+        onChange={handleChange}
         row
-        aria-labelledby="row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
       >
-        <FormControlLabel
-          value={true}
-          onClick={handleOnClick}
-          control={<Radio />}
-          label="Correct"
-          checked={(!change && false) || (change && checked)}
-        />
-        <FormControlLabel
-          value={false}
-          checked={(!change && false) || (change && !checked)}
-          onClick={handleOnClick}
-          control={<Radio />}
-          label="Incorrect"
-        />
+        <FormControlLabel value={true} control={<Radio />} label="Correct" />
+        <FormControlLabel value={false} control={<Radio />} label="Incorrect" />
       </RadioGroup>
     </FormControl>
   );
